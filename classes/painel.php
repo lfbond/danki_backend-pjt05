f9e340fa5daa23ca6e3e21d412f4b663d5889fe3
Luis Felipe de Souza Bond <69223872+lfbond@users.noreply.github.com>
2022-04-26 21:45:29 -0300

    Add files via upload

---
+++ classes/painel.php
@@ -0,0 +1,13 @@
+<?php
+	class Painel {
+
+		public static function logado(){
+			return isset($_SESSION['login']) ? true : false;
+		}
+
+		public static function loggout(){
+			session_destroy();
+			header('location: '.INCLUDE_PATH_PAINEL);
+		}
+	}
+?>
